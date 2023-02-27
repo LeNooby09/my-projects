8fe7febd09840e4502a3bd14edafcf6162b34cc3
LeNooby 09 <phillihp.steingraeber@gmail.com>
2023-02-27 14:53:27 +0100

    cleaned up code

--- c/sqrt27 project/calculate.c
+++ c/sqrt27 project/calculate.c
@@ -4,14 +4,13 @@
 int main()
 {
     mpfr_t x;
-    mpfr_init2(x, 100000); // initialize with 500-bit precision max10000000000
+    mpfr_init2(x, 100000); //precision max10000000000
 
-    mpfr_set_ui(x, 27, MPFR_RNDN); // set x to 27
-    mpfr_sqrt(x, x, MPFR_RNDN); // calculate the square root of x
+    mpfr_set_ui(x, 27, MPFR_RNDN);
+    mpfr_sqrt(x, x, MPFR_RNDN);
 
-    // print the result to 500 decimal places
     mpfr_printf("\n%.100000Rf\n\n", x);
 
-    mpfr_clear(x); // free memory
+    mpfr_clear(x);
     return 0;
 }
